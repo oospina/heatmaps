2089f0e4769733f7e4f6cf298590d1be40752d3e
oscaredd <oscaredd_ol@yahoo.es>
2020-07-26 22:31:38 -0400

    Add files via upload

--- heatmap_code.R
+++ heatmap_code.R
@@ -1,71 +1,56 @@
+library("tidyverse")
 library("gplots")
 library("RColorBrewer")
 
-setwd("/Users/oscar/Downloads/Lemmon-etal_ReinforcementPaper")
+# Change the filepath of the tab-separated data
+df_fpath <- "./HetProfile_0.5_16_Filtered_pHet.txt"
 
-data <- read.table("HetProfile_0.5_16_Filtered2_pHet_NaN.txt")
+# Read the file containing the data. Convert first column to row names.
+df <- read_delim(df_fpath, delim="\t")
+df <- column_to_rownames(df, var = "X1")
 
-dataRowMeans <- cbind(data, rowMeans(data, na.rm=T))
-colnames(dataRowMeans)[ncol(dataRowMeans)] <- "RMean"
-dataRowMeans <- dataRowMeans[order(dataRowMeans$RMean), ]
-dataRowMeans <- dataRowMeans[ , 1:ncol(data)]
+# Convert -1.0 values to NAs.
+df[df == -1] <- NA
 
-dataColMeans <- rbind(data, colMeans(data, na.rm=T))
-rownames(dataColMeans)[nrow(dataColMeans)] <- "CMean"
-dataColMeans <- dataColMeans[ , order(dataColMeans[nrow(dataColMeans),])]
-dataColMeans <- dataColMeans[1:nrow(data), ]
+# Calculate row means and attach to data frame to use for sorting.
+df_SortRows <- df
+df_SortRows$row_means <- rowMeans(df_SortRows, na.rm=T)
+# write.table(df_SortRows, "/Users/OSCAREOSPINATOBON/Desktop/tableRows.csv", sep=',', quote=F)
+df_SortRows <- df_SortRows[order(df_SortRows$row_means), ]
+# write.table(df_SortRows, "/Users/OSCAREOSPINATOBON/Desktop/tableRows2.csv", sep=',', quote=F)
 
-dataBothMeans <- dataColMeans
-dataBothMeans <- cbind(dataBothMeans, rowMeans(dataBothMeans , na.rm=T))
-colnames(dataBothMeans)[ncol(dataBothMeans)] <- "RMean"
-dataBothMeans <- dataBothMeans[order(dataBothMeans$RMean), ]
-dataBothMeans <- dataBothMeans[ , 1:ncol(data)]
+# Calculate column means and attach to data frame to use for sorting.
+df_SortCols <- df
+df_SortCols <- rbind(df_SortCols, colMeans(df_SortCols, na.rm=T))
+rownames(df_SortCols)[length(rownames(df_SortCols))] <- "col_means"
+# write.table(df_SortCols, "/Users/OSCAREOSPINATOBON/Desktop/tableCols.csv", sep=',', quote=F)
+df_SortCols <- df_SortCols[, order(df_SortCols[nrow(df_SortCols), ])]
+# write.table(df_SortCols, "/Users/OSCAREOSPINATOBON/Desktop/tableCols2.csv", sep=',', quote=F)
 
-data <- as.matrix(data)
-dataRowMeans <- as.matrix(dataRowMeans)
-dataColMeans <- as.matrix(dataColMeans)
-dataBothMeans <- as.matrix(dataBothMeans)
+# Calculate column and row means and attach to data frame to use for sorting.
+df_SortTwoWay <- df
+df_SortTwoWay$row_means <- rowMeans(df_SortTwoWay, na.rm=T)
+df_SortTwoWay <- rbind(df_SortTwoWay, colMeans(df_SortTwoWay, na.rm=T))
+rownames(df_SortTwoWay)[length(rownames(df_SortTwoWay))] <- "col_means"
+# write.table(df_SortTwoWay, "/Users/OSCAREOSPINATOBON/Desktop/tableBoth.csv", sep=',', quote=F)
+df_SortTwoWay <- df_SortTwoWay[, order(df_SortTwoWay[nrow(df_SortTwoWay), ])]
+df_SortTwoWay <- df_SortTwoWay[order(df_SortTwoWay[, ncol(df_SortTwoWay)]), ]
+# write.table(df_SortTwoWay, "/Users/OSCAREOSPINATOBON/Desktop/tableBoth2.csv", sep=',', quote=F)
 
+# Set graphical parameters for heatmaps.
 lmat <- rbind(c(0,0),c(0,1))
 lwid <- c(0.1,10)
 lhei <- c(0.1,10)
-
 my_palette <- colorRampPalette(c("green", "black"))(n = 59)
 
-#my_palette <- colorRampPalette(c("white", "green", "black"))(n = 59)
-
-#col_breaks = c(seq(-1,-0.1,length=20), # for white (NAs)
-#			seq(0,0.5,length=20),  # for green
-#			seq(0.51,1,length=20)) # for black
-
-pdf(file="HetProfile_0.5_16_Filtered_pHet.pdf", width = 400, height = 400, pointsize = 100)
-
-#heatmap.2(data, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, breaks=col_breaks)
-
-heatmap.2(data, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
-
+pdf(file="./HetProfile_0.5_16_Filtered_pHet_SortRows.pdf", width = 400, height = 400, pointsize = 100)
+heatmap.2(data.matrix(df_SortRows), Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
 graphics.off()
 
-pdf(file="HetProfile_0.5_16_Filtered_pHet_ROW.pdf", width = 400, height = 400, pointsize = 100)
-
-#heatmap.2(dataRowMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, breaks=col_breaks)
-
-heatmap.2(dataRowMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
-
+pdf(file="HetProfile_0.5_16_Filtered_pHet_SortCols.pdf", width = 400, height = 400, pointsize = 100)
+heatmap.2(data.matrix(df_SortCols), Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
 graphics.off()
 
-pdf(file="HetProfile_0.5_16_Filtered_pHet_COL.pdf", width = 400, height = 400, pointsize = 100)
-
-#heatmap.2(dataColMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, breaks=col_breaks)
-
-heatmap.2(dataColMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
-
-graphics.off()
-
-pdf(file="HetProfile_0.5_16_Filtered_pHet_BOTH.pdf", width = 400, height = 400, pointsize = 100)
-
-#heatmap.2(dataBothMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, breaks=col_breaks)
-
-heatmap.2(dataBothMeans, Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
-
+pdf(file="HetProfile_0.5_16_Filtered_pHet_SortBoth.pdf", width = 400, height = 400, pointsize = 100)
+heatmap.2(data.matrix(df_SortTwoWay), Colv=F, Rowv=F, key=F, dendrogram="none", col=my_palette, lmat=lmat, lwid=lwid, lhei=lhei, mar=c(3, 3), trace="none", offsetRow=0.1, offsetCol=0.1, na.color="white")
 graphics.off()
